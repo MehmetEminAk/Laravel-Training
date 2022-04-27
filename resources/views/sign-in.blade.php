c82754614359500d1e01534e72c65cbb40be4d01
Mehmet Emin Ak <mak44162@gmail.com>
2022-04-27 16:22:43 +0300

    Sign-in Sayfasına form Eklendi

---
+++ resources/views/sign-in.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Giriş Yap</title>
+</head>
+<body>
+    <form>
+        
+        <input type="email" name="loginmail" id="maillogin" placeholder="E-mail adresinizi girin:"/><br>
+        <input type="password" name="loginmail" id="maillogin" placeholder="Şifrenizi girin"/><br>
+        <button  type="submit" value="Giriş Yap"></button>
+
+    </form>
+
+</body>
+</html>
